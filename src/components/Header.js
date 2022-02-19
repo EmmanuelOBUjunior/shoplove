e1b9f7797e106cbb49a6da9e7c3b8804b76723bb
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-19 02:48:46 +0000

    installing packages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {SearchOutlined} from '@mui/icons-material'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+        <nav>
+        <ul className="ul-items">
+                <li className="li-items">
+                    <Link to = '/women'>Women</Link>
+                    <Link to = '/men'>Men</Link>
+                    <Link to = '/crispin'>Crispin</Link>
+                    <Link to = '/shop'>The Gift Shop</Link>
+                    <Link to = '/about'>About</Link>
+                </li>
+            </ul>
+            <div className="logo">
+                <h1>ShopLiVe</h1>
+            </div>
+            <div className="right">
+                <div className="search">
+                    <SearchOutlined className= 'search-icon'/>
+                    <button className= "sch-btn">Search</button>
+                </div>
+            </div>
+        </nav>
+    </div>
   )
 }
 
