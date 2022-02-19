da6f7bfbf06965a2d152fb10dd0c6dd8708e13bd
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-19 13:59:23 +0000

    Updating section with contents

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,13 @@ import '../styled/Section.scss'
 const Section = () => {
   return (
     <section className='main-section'>
-
+        <div className="contents">
+            <h1>Look Great, Smell Good, Feel Sassy</h1>
+            <button>Explore</button>
+        </div>
+        <div className="bottom-text">
+            <p>10% off for all our products</p>
+        </div>
     </section>
   )
 }
