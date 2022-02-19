13264e23f77f15a611a4066fbb665398b99f31a7
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-19 15:26:19 +0000

    Small section.js

--- src/components/SmallSection.js
+++ src/components/SmallSection.js
@@ -3,7 +3,31 @@ import React from 'react'
 const SmallSection = () => {
   return (
     <section className="second-section">
-        
+        <div className="allcart">
+            <div className="cart">
+                <img src="https://images.unsplash.com/photo-1578939662863-5cd416d45a69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1107&q=80" alt="unisex" />
+                <p>Unisex</p>
+                <h1>Lorem ipsum dolor hea</h1>
+            </div>
+
+            <div className="cart">
+                <img src="https://images.unsplash.com/photo-1453486030486-0a5ffcd82cd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=663&q=80" alt="unisex" />
+                <p>Unisex</p>
+                <h1>Lorem ipsum dolor hea</h1>
+            </div>
+
+            <div className="cart">
+                <img src="https://images.unsplash.com/photo-1520006403909-838d6b92c22e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="women" />
+                <p>Women</p>
+                <h1>Lorem ipsum dolor hea</h1>
+            </div>
+
+            <div className="cart">
+                <img src="https://images.unsplash.com/photo-1561053720-76cd73ff22c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHVuaXNleCUyMGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="men" />
+                <p>Men</p>
+                <h1>Lorem ipsum dolor hea</h1>
+            </div>
+        </div>
     </section>
   )
 }
