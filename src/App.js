d2ac30765417edd42dcb7984fa255e22943fc418
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-19 13:51:22 +0000

    styling main-section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
 import './App.scss';
+import Section from './components/Section'
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Section/>
     </div>
   );
 }
