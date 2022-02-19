287be9c4ded20d0d559ac874c4118ba24a97290a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-19 04:08:53 +0000

    styling search container

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
                     <Link to = '/about'>About</Link>
                 </li>
             </ul>
-            <div className="logo">
+            <div className="logo"> 
                 <h1>ShopLiVe</h1>
             </div>
             <div className="right">
