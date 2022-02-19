7dc5b37e2ed9e199de9b033d918a3ede486ea329
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-19 15:32:42 +0000

    Importing smallsection in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from './components/Header'
 import './App.scss';
 import Section from './components/Section'
+import SmallSection from './components/SmallSection'
+
 
 function App() {
   return (
     <div className="App">
      <Header/>
      <Section/>
+     <SmallSection/>
     </div>
   );
 }
