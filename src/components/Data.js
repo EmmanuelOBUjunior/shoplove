8bef8b27ca4fce1ec21abace3ccc942071fa9b37
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-21 11:28:43 +0000

    Added Data.js file

---
+++ src/components/Data.js
@@ -0,0 +1,9 @@
+export const Data =[
+    {
+        title:'Hear from our customers',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis accusantium quam magni, excepturi aperiam repellendus corrupti modi vel, ad, repellat inventore libero!',
+        image: 'https://images.pexels.com/photos/9167333/pexels-photo-9167333.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        button: 'Shop With Us',
+        className: 'data-item'
+    },
+]
