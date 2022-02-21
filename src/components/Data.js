7a81526d358e91f60b3ada24fe371b3482822c83
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-21 11:30:58 +0000

    inserting information into Data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -6,4 +6,11 @@ export const Data =[
         button: 'Shop With Us',
         className: 'data-item'
     },
+    {
+        title: 'Hear from our customers',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae eius distinctio ratione fugiat, iusto iure, doloremque dignissimos eum aliquam, ad fugit tempore?',
+        image: 'https://img.freepik.com/free-photo/amused-pretty-girl-with-curly-afro-hair-raises-palms-has-cheerful-expression-smiles-broadly-sees-something-funny-wears-white-sweater_273609-43437.jpg?w=740',
+        button: 'Shop With Us',
+        className: 'data-item'
+    }
 ]
